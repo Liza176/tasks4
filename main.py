b1f299ed9511a1630cdc637e7739b699b5c24148
Liza176 <138390166+Liza176@users.noreply.github.com>
2023-08-02 15:43:20 +0600

    Update main.py

--- main.py
+++ main.py
@@ -18,81 +18,6 @@ import random
 # else:
 #     print('Пожалуйста напишите Ножницы, Камень или Бумага')
 
-
-rand = random.randint(2,11)
-if rand == 5:
-        rand = random.randint(2,11)
-you = rand
-
-print (you)
-
-
-# 
-
-
-rand = random.randint(2,11)
-if rand == 5:
-        rand = random.randint(2,11)
-dil = rand
-
-
-while True:
-    pass_more = input('Хватит или Еще? ')
-    if pass_more == 'Еще':
-        rand = random.randint(2,11)
-        if rand == 5:
-            rand = random.randint(2,11)
-        you += rand
-        print (you)
-        # last = rand
-        rand = random.randint(2,11)
-        if rand == 5:
-            rand = random.randint(2,11)
-        dil += rand
-    elif pass_more == 'Хватит':
-        break
-
-    if you > 21:
-        print('not win')
-        break
-
-    if dil > 21:
-        print('win')
-        break
-
-        
-if pass_more == 'Хватит':
-    print (f'Ваши очки {you}')
-    print (f'Очки дилера {dil}')
-
-    if you > dil :
-        print('win')
-    else:
-        print('not win')
-        
-
- # Второй варриант
-
-import random
-
-# r = input()
-# mas = ['Ножницы','Камень','Бумага']
-# random.shuffle(mas)
-
-# q =mas[0]
-
-# if q == r:
-#     print('Ничья , компьютер выбрал' , q)
-
-# elif q == 'Ножницы' and r == 'Бумага' or q == 'Камень' and r =='Ножницы'  or q == 'Бумага' and r =='Камень':
-#     print('Вы проиграли , компьютер выбрал' , q)
-
-
-# elif q == 'Ножницы' and r == 'Камень' or q == 'Камень' and r =='Бумага'  or q == 'Бумага' and r =='Ножницы':
-#     print('Вы выиграли , компьютер выбрал' , q)
-# else:
-#     print('Пожалуйста напишите Ножницы, Камень или Бумага')
-
 mas = []
 rand = random.randint(2,11)
 if rand == 5:
@@ -128,10 +53,14 @@ while True:
         break
 
     if you > 21:
+        print (f'Ваши очки {you}')
+        print (f'Очки дилера {dil}')
         print('not win')
         break
 
     if dil > 21:
+        print (f'Ваши очки {you}')
+        print (f'Очки дилера {dil}')
         print('win')
         break
 
@@ -144,6 +73,14 @@ if pass_more == 'Хватит':
         print('win')
     else:
         print('not win')
+
+
+
+with open('tex.txt','a',encoding='utf-8') as file:
+    if you > dil :
+        print(file.write(f'Вы выйграли со счетом {you}:{dil}'+ '\n'))
+    else:
+        print(file.write(f'Компьютер выйграл со счетом {dil}:{you}'+ '\n'))
 
 
 
